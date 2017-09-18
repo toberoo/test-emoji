832ee99397ce2a6788cc3dd0c4c132e226c0e8d6
Shehroze Khan <shehroze@upside.com>
2017-09-18 14:45:08 -0400

    Added buttons

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import chalk from 'chalk';
 import { get, emojify } from 'node-emoji';
 
+const cat = get('cat');
+const taco = get('taco');
+
 const input = document.getElementById('emoji-input');
+const catButton = document.getElementById('cat-emoji');
+const tacoButton = document.getElementById('taco-emoji');
+
+catButton.innerHTML = cat;
+tacoButton.innerHTML = taco;
+
+catButton.addEventListener('click', (e) => {
+    input.value = input.value + cat + ' ';
+});
+
+tacoButton.addEventListener('click', (e) => {
+    input.value = input.value + taco + ' ';
+});
 
 input.addEventListener("input", function (e) {
     this.value = emojify(this.value);
